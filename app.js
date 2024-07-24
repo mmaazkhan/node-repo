155beeea09e0b082cb0cacaf4b35a3ac7e3a03e2
mmaazkhna <maazkotha007@gmail.com>
2024-07-24 12:10:40 +0500

    add files

---
+++ app.js
@@ -0,0 +1,7 @@
+// app.js
+
+// Print "Hello, World!"
+console.log("Hello, World!");
+
+// Print a paragraph
+console.log("This is a simple paragraph printed by a Node.js script. Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine. It allows you to run JavaScript code outside of a web browser.");
